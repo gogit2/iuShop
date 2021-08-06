97217c61a5ef69a91ed316dc44e6972c2103a5fb
gogit2 <gogit2hd@gmail.com>
2021-08-06 19:30:32 +0200

    Initial commit

---
+++ iuShopWebParent/iuShopBackEnd/src/main/java/com/iushop/admin/IuShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.iushop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuShopBackEndApplication.class, args);
+	}
+
+}
---
+++ iuShopWebParent/iuShopBackEnd/src/main/java/com/iushop/admin/MainController.java
@@ -0,0 +1,18 @@
+package com.iushop.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String main(){
+        return "index";
+    }
+
+    @GetMapping("/h")
+    public String h2(){
+        return "templates/index.html";
+    }
+}
