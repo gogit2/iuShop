1e0f1a75111db8342b496d00f8c16770d22f93e4
gogit2 <gogit2hd@gmail.com>
2022-07-15 18:20:28 +0200

    Add-Export-UsersToExcel
    Add AbstractExporter class to reduce repetition

---
+++ iuShopWebParent/iuShopBackEnd/src/main/java/com/iushop/admin/user/AbstractExporter.java
@@ -0,0 +1,25 @@
+package com.iushop.admin.user;
+
+import com.iushop.common.entity.User;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class AbstractExporter {
+
+    public void setResponseHeader(HttpServletResponse response, String extension, String contentType) throws IOException {
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss");
+        String timeStamp = dateFormatter.format(new Date());
+        String fileName = "users_" + timeStamp + extension;
+
+        response.setContentType(contentType);
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + fileName;
+        response.setHeader(headerKey, headerValue);
+    }
+}
--- iuShopWebParent/iuShopBackEnd/src/main/java/com/iushop/admin/user/UserCsvExporter.java
+++ iuShopWebParent/iuShopBackEnd/src/main/java/com/iushop/admin/user/UserCsvExporter.java
@@ -13,18 +13,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter{
 
     public void export(List<User> usersList, HttpServletResponse response) throws IOException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-mm-dd_hh-mm-ss");
-        String timeStamp = dateFormatter.format(new Date());
-        String fileName = "users_" + timeStamp + ".csv";
-
-        response.setContentType("text/csv");
-
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=" + fileName;
-        response.setHeader(headerKey, headerValue);
+        super.setResponseHeader(response, ".csv", "text/csv");
 
         ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
 
